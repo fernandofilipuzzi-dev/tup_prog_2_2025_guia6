597738be6dc8b319b10846cb50ef9773be75303b
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2025-09-17 20:50:37 -0300

    iniciando repo

---
+++ Guia6/Ejercicio1/Models/PersonaJuridica.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Ejercicio1.Models;
+
+public class PersonaJuridica:Persona
+{
+    protected string cuit;
+ 
+    public PersonaJuridica(string nombre, string cuit):base(nombre)
+    {
+        Match match = Regex.Match(cuit, @"\d{11}");
+        if (match.Success == false)
+        {
+            throw new FormatoCUITNoValidoException();
+        }
+
+        this.cuit = cuit;
+    }
+    
+    override public string Describir()
+    {
+        return $"{nombre}  ( {cuit} )";
+    }
+}
